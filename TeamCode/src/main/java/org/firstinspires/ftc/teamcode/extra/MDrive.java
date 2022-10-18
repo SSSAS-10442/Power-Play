d8275b1ca4696e25c6be9daa0aa201fe62c538f7
JackIsSSSASy <74074379+JackIsSSSASy@users.noreply.github.com>
2022-10-18 16:07:21 -0400

    Update MDrive.java
    --MDrive--
    • Changed drive strafe and rotate
    • Added MAIN and MOTOR states
    • MAIN state contains previous code
    • MOTOR allows running motors one by one

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/MDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/MDrive.java
@@ -1,12 +1,21 @@
 package org.firstinspires.ftc.teamcode.extra;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.main.MotorsEx;
-
+@TeleOp
 public class MDrive extends OpMode {
+
+    enum State {
+        MAIN,
+        MOTOR,
+        ;
+    }
+
+    State state;
 
     DcMotorEx lf = null;
     DcMotorEx rf = null;
@@ -22,6 +31,11 @@ public class MDrive extends OpMode {
     double rightBackPower = 0;
     double leftBackPower = 0;
 
+    private Servo claw = null;
+    private double clawPos = 0.5;
+    private boolean aLastFrame = false;
+    private boolean bLastFrame = false;
+
     @Override
     public void init() {
 
@@ -31,9 +45,13 @@ public class MDrive extends OpMode {
         lb = hardwareMap.get(DcMotorEx.class, "leftBack");
 
         lf.setDirection(DcMotorSimple.Direction.FORWARD);
+        lb.setDirection(DcMotorSimple.Direction.FORWARD);
         rf.setDirection(DcMotorSimple.Direction.REVERSE);
-        rb.setDirection(DcMotorSimple.Direction.FORWARD);
-        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        state = State.MAIN;
 
     }
 
@@ -50,20 +68,67 @@ public class MDrive extends OpMode {
     @Override
     public void loop() {
 
-        drive = gamepad1.left_stick_y * 2000;
-        strafe = gamepad1.left_stick_x * 2000;
-        rotate = gamepad1.right_stick_x * 2000;
+        telemetry.addLine("State: " + state);
 
-        leftFrontPower = rotate - strafe + drive;
-        rightFrontPower = rotate - strafe - drive;
-        leftBackPower = rotate + strafe + drive;
-        rightBackPower = rotate + strafe - drive;
+        switch (state) {
+            case MAIN:
+                drive = gamepad1.left_stick_y;
+                strafe = gamepad1.left_stick_x;
+                rotate = gamepad1.right_stick_x;
 
-        lf.setVelocity(leftFrontPower);
-        rf.setVelocity(rightFrontPower);
-        rb.setVelocity(rightBackPower);
-        lb.setVelocity(leftBackPower);
-        
+                leftFrontPower = drive + rotate + strafe;
+                leftBackPower = drive + rotate - strafe;
+                rightFrontPower = drive - rotate - strafe;
+                rightBackPower = drive - rotate + strafe;
+
+                lf.setVelocity(leftFrontPower);
+                lb.setVelocity(leftBackPower);
+                rf.setVelocity(rightFrontPower);
+                rb.setVelocity(rightBackPower);
+
+                if (gamepad1.a && !aLastFrame) {
+                    clawPos += 0.05;
+                }
+                if (gamepad1.b && !bLastFrame) {
+                    clawPos -= 0.05;
+                }
+
+                claw.setPosition(clawPos);
+
+                aLastFrame = gamepad1.a;
+                bLastFrame = gamepad1.b;
+
+                if (gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right) {
+                    state = State.MOTOR;
+                }
+
+                break;
+            case MOTOR:
+
+                if (gamepad1.dpad_down) {
+                    lf.setVelocity(2000);
+                } if (gamepad1.dpad_left) {
+                    lb.setVelocity(2000);
+                } if (gamepad1.dpad_up) {
+                    rf.setVelocity(2000);
+                } if (gamepad1.dpad_right) {
+                    rb.setVelocity(2000);
+                }
+
+                if (gamepad1.start) {
+                    state = State.MAIN;
+                }
+
+                telemetry.addLine("Down/LF: " + gamepad1.dpad_down);
+                telemetry.addLine("Left/LB: " + gamepad1.dpad_left);
+                telemetry.addLine("Up/RF: " + gamepad1.dpad_up);
+                telemetry.addLine("Right/RB: " + gamepad1.dpad_right);
+
+                break;
+        }
+
+        telemetry.update();
+
     }
 
     @Override
