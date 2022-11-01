048b61e156a8a828594caf4590bc80a074f2fe57
JackIsSSSASy <74074379+JackIsSSSASy@users.noreply.github.com>
2022-11-01 17:12:34 -0400

    Update Main.java
    Changed arm values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Main.java
@@ -14,10 +14,10 @@ public abstract class Main extends OpMode {
 
     public static Version version = null;
 
-    static int ARM_SCORING_L = 2000;
+    static int ARM_SCORING_L = 1500;
     static int ARM_SCORING_M = 1400;
     static int ARM_SCORING_S = 700;
-    static int ARM_SCORING_GROUND = 100;
+    static int ARM_SCORING_GROUND = 200;
 
     static int LIFT_SCORING_L = 1400;
     static int LIFT_SCORING_M = 900;
